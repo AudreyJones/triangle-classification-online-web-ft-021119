b7bd53604da13af9d954fc4acb5681c05ae65c98
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-08 20:24:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
+# The sum of the lengths of any two sides of a triangle always exceeds the length
+# of the third side. This is a principle known as the _triangle inequality_.
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+
+  def kind
+    # if first + second
+    if first = second = third
+      returns :equilateral
+    elsif
+      returns :isosceles
+    elsif
+      returns :scalene
+    else
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+
+  # class TriangleError < StandardError
+  #   def message
+  #     "Your triangle is invalid"
+  #   end
+  # end
 end
